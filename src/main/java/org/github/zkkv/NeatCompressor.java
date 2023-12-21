efc98d28a3712c5d7c8f4fe2ed22d42dec39308c
zkkv <zkkv@proton.me>
2023-12-21 02:04:36 +0100

    Setup classes

---
+++ src/main/java/org/github/zkkv/NeatCompressor.java
@@ -0,0 +1,14 @@
+package org.github.zkkv;
+
+public class NeatCompressor {
+
+    private NeatCompressor() {};
+
+    static String compress(String uncompressed) {
+        return null;
+    }
+
+    static String decompress(String compressed) {
+        return null;
+    }
+}
